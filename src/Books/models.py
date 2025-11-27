6f2c6f9193554cc07f36ad26ff73278357ad7eba
Mohammad-Amirifard <amirifard.mo@gmail.com>
2025-11-27 18:00:17 +0100

    Db structure for books added

---
+++ src/Books/models.py
@@ -0,0 +1,29 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
+from datetime import datetime
+
+class Book(SQLModel , table=True):
+    __tablename__ = "books"
+
+    uid:uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            unique=True,
+            nullable=False
+        )
+    )
+
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language:str
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at:datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+
+    # This following code  is optional, but it helps when we print the object
+    def __repr__(self) -> str:
+        return f"<Book {self.title}>"
